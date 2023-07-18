9b6a33d8855389c0aae172c28f57950d47edf7e6
Olena Stasiuk <51089449+estasjuk@users.noreply.github.com>
2023-07-18 16:11:25 +0300

    MAR-176, filter products in the category by "new" or "used" and by price range, upd swagger (#7)

--- controllers/notices.js
+++ controllers/notices.js
@@ -1,6 +1,8 @@
 const { Notice } = require("../db/models/notices");
 const HttpError = require("../helpers/httpError");
 const controllerWrapper = require("../utils/controllerWrapper");
+
+const buildFilterObject = require("../utils/filterObject");
 const buildSortObject = require("../utils/sortObject");
 
 const getAllNotices = async (req, res) => {
@@ -29,15 +31,17 @@ const getAllNotices = async (req, res) => {
 };
 
 const getNoticesByCategory = async (req, res) => {
-  const { page = 1, limit = 9, sort = "newest"} = req.query;
+
+  const { page = 1, limit = 9, sort = "newest", goodtype, priceRange} = req.query;
   const { category } = req.params;
   const skip = (page - 1) * limit;
+  const query = { category, goodtype, priceRange };
 
-    const result = await Notice.find({ category }, "", {
+  const result = await Notice.find(buildFilterObject(query), "", {
       skip,
       limit: Number(limit),
-    }).sort(buildSortObject(sort));
- 
+  }).sort({ createdAt: -1 });
+  
     if (result.length === 0) {
       throw HttpError.NotFoundError("Notices not found");
     }
--- routes/api/notices/notices-routes.js
+++ routes/api/notices/notices-routes.js
@@ -4,16 +4,13 @@ const express = require('express');
 const validateBody = require('../../../utils/validateBody');
 const upload = require("../../../utils/upload");
 
-
 const { addNotice, getNoticesByCategory, getAllNotices } = require('../../../controllers/notices');
 const { addNoticeSchema} = require('../../../db/models/notices');
 
 const router = express.Router();
 
-
 router.get('/', getAllNotices);
 router.get('/:category', getNoticesByCategory);
 router.post('/', upload.array('photos', 10), validateBody(addNoticeSchema), addNotice);
-
 
 module.exports = router;
