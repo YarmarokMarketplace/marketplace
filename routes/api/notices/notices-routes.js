b5340b74a92172957f7f3cda7a6c8c0c58c07311
Olena Stasiuk <51089449+estasjuk@users.noreply.github.com>
2023-07-26 15:36:41 +0300

    mar-187, add updateNotice endpoint, upd swagger (#11)

--- routes/api/notices/notices-routes.js
+++ routes/api/notices/notices-routes.js
@@ -3,14 +3,16 @@ require("dotenv").config();
 const express = require('express');
 const validateBody = require('../../../utils/validateBody');
 const upload = require("../../../utils/upload");
+const isValidId = require("../../../middlewares/isValidId");
 
-const { addNotice, getNoticesByCategory, getAllNotices } = require('../../../controllers/notices');
-const { addNoticeSchema} = require('../../../db/models/notices');
+const { addNotice, getNoticesByCategory, getAllNotices, updateNotice } = require('../../../controllers/notices');
+const { addNoticeSchema, updateNoticeSchema} = require('../../../db/models/notices');
 
 const router = express.Router();
 
 router.get('/', getAllNotices);
 router.get('/:category', getNoticesByCategory);
-router.post('/', upload.array('photos', 10), validateBody(addNoticeSchema), addNotice);
+router.post('/', upload.array('photos', 6), validateBody(addNoticeSchema), addNotice);
+router.patch('/:id', isValidId, validateBody(updateNoticeSchema), upload.array('photos', 6), updateNotice);
 
 module.exports = router;
