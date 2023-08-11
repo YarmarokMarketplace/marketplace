7c9840d505e5de081d563d3ea2e0d657858a6418
Olena Stasiuk <estasjuk@gmail.com>
2023-08-11 14:49:56 +0300

    corr accessToken expireTime

--- controllers/auth.js
+++ controllers/auth.js
@@ -105,7 +105,7 @@ const login = async (req, res) => {
         id: user._id,
     };
 
-    const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, { expiresIn: "15m" });
+    const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, { expiresIn: "15s" });
     const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, {expiresIn: "7d"});
     await User.findOneAndUpdate({ _id: payload.id }, { $set: { accessToken, refreshToken } });
 
