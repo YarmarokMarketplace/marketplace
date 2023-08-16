d42cc76de18ad80f9b96cd6406b0aac167111a62
Olena Stasiuk <estasjuk@gmail.com>
2023-08-17 01:11:17 +0300

    add owner-ref to the notice model

--- db/models/notices.js
+++ db/models/notices.js
@@ -39,6 +39,10 @@ const noticeSchema = new Schema(
     active: {
       type: Boolean,
       default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
     }
   },
   { versionKey: false, timestamps: true }
