ec33b7f405652f913c1e10962a4affb9090eecd9
Olena Stasiuk <estasjuk@gmail.com>
2023-08-19 01:12:43 +0300

    add auth check for addNotice, updateNotice, deleteNotice and toggleActive, upd swagger

--- routes/api/notices/notices-routes.js
+++ routes/api/notices/notices-routes.js
@@ -12,10 +12,10 @@ const router = express.Router();
 
 router.get('/', getAllNotices);
 router.get('/:category', getNoticesByCategory);
-router.post('/', upload.array('photos', 6), validateBody(addNoticeSchema), addNotice);
-router.patch('/:id', isValidId, validateBody(updateNoticeSchema), upload.array('photos', 6), updateNotice);
-router.delete('/notice/:id', isValidId, removeNotice);
+router.post('/', authenticate, upload.array('photos', 6), validateBody(addNoticeSchema), addNotice);
+router.patch('/:id', authenticate, isValidId, validateBody(updateNoticeSchema), upload.array('photos', 6), updateNotice);
+router.delete('/notice/:id', authenticate, isValidId, removeNotice);
 router.get('/notice/:id', isValidId, getNoticeById);
-router.patch('/notice/:id/active', isValidId, validateBody(toggleActiveSchema), toggleActive);
+router.patch('/notice/:id/active', authenticate, isValidId, validateBody(toggleActiveSchema), toggleActive);
 
 module.exports = router;
