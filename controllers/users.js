db02472ae8da9fcf76c34e09624ef399b8bb6710
Olena Stasiuk <estasjuk@gmail.com>
2023-09-02 01:46:34 +0300

    feature/mar-275, add updateUserData endpoint, upd swagger

--- controllers/users.js
+++ controllers/users.js
@@ -15,6 +15,39 @@ const removeUser = async (req, res) => {
         }});
 };
 
+const updateUserData = async (req, res) => {
+    const { _id } = req.user;
+    const userData = req.body;
+    let data;
+    if (req.file) {
+        const uploaded = req.file.location;
+        data = { ...userData, avatarURL: uploaded}
+    } else {
+        data = { ...userData }
+    }
+    console.log(data)
+    
+    const result = await User.findByIdAndUpdate(_id, data, { new: true });
+    
+    if (!result) {
+        throw new HttpError(404, 'User not found');
+    }
+    res.status(201).json({
+        status: 'success',
+        code: 201,
+        user: {
+            _id: result.id,
+            email: result.email,
+            name: result.name,
+            lastname: result.lastname,
+            patronymic: result.patronymic,
+            avatarURL: result.avatarURL,
+            phone: result.phone,
+        }
+    });
+};
+
 module.exports = {
     removeUser: controllerWrapper(removeUser),
+    updateUserData: controllerWrapper(updateUserData),
 };
--- routes/api/user/user-routes.js
+++ routes/api/user/user-routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const authenticate = require('../../../middlewares/authenticate');
 const isValidId = require('../../../middlewares/isValidId');
+const upload = require('../../../utils/upload');
+const validateBody = require('../../../utils/validateBody');
 
-const { removeUser} = require('../../../controllers/users');
+const { removeUser, updateUserData } = require('../../../controllers/users');
+const { updateSchema } = require('../../../db/models/users');
 
 const router = express.Router();
 
 router.delete('/:id', authenticate, isValidId, removeUser);
+router.patch('/:id', authenticate, isValidId, upload.single('avatarURL'), validateBody(updateSchema), updateUserData);
 
 module.exports = router;
