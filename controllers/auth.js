2fca491684592f90a0c934e4a7b744a74cf60f7f
Olena Stasiuk <estasjuk@gmail.com>
2023-09-09 16:50:56 +0300

    corr res from login request

--- controllers/auth.js
+++ controllers/auth.js
@@ -183,6 +183,10 @@ const login = async (req, res) => {
             id: user._id,
             email: user.email,
             name: user.name,
+            lastname: user.lastname,
+            patronymic: user.patronymic,
+            avatarURL: user.avatarURL,
+            phone: user.phone,
         }
     });
 };
